840bd61a143304beca1e85634a566b2e36cac6c0
Ryan Barnes <rybarnes@gmail.com>
2021-03-29 16:21:45 -0400

    styling on firebaseui auth functional

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,42 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <h1>Welcome to My Awesome App</h1>
-    <div id="firebaseui-auth-container"></div>
-    <div id="loader">Loading...</div>
+    <q-card>
+      <q-card-section>
+        <div class="firebaseui-auth-container"></div>
+        <div id="loader">Loading...</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 import firebase from 'firebase'
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'index',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
+
 
 export default {
   name: 'PageIndex'
 }
 </script>
+
+<style>
+@import 'https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.css'
+</style>
+
