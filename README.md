506e941f0fad0c7480512ca9bf6c572216385fdf
Ryan Barnes <rybarnes@gmail.com>
2021-03-29 10:44:05 -0400

    first commit, auth scaffolded

---
+++ README.md
@@ -0,0 +1,45 @@
+# Tag Journal (tagjournal)
+
+A simple, fast journal app.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://v1.quasar.dev/quasar-cli/quasar-conf-js).
+
+
+### Build Log
+#### 03/29/21
+Starting with authorization with firebaseui. Installed, and visable, but hella ugly. Need to clean it up.
+
+Schema: 
+User > Entires > Date, Tag list
+User > Tags
+
+Pages:
+Login
+Journal
+Tags
+
+Components: 
+tagEntry
+journal
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Welcome to My Awesome App</h1>
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
+  </q-page>
+</template>
+
+<script>
+import firebase from 'firebase'
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'index',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+export default {
+  name: 'PageIndex'
+}
+</script>
